a9c895dbbe962c28c7ea833d585c8ba6de7f4b8c
Sven Andre Svensson <sven+git@bringsrud.net>
2021-07-14 17:47:56 +0200

    Initial Commit

---
+++ SwiftUINavigationFailiurSecondTime/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUINavigationFailiurSecondTime
+//
+//  Created by Sven Svensson on 14/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUINavigationFailiurSecondTime/SwiftUINavigationFailiurSecondTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationFailiurSecondTimeApp.swift
+//  SwiftUINavigationFailiurSecondTime
+//
+//  Created by Sven Svensson on 14/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationFailiurSecondTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
